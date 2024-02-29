f89cb0ba9cf52896fc0bf9bed37797946dd6f942
cjgonzalezlaffont <cjgonzalezlaffont@gmail.com>
2024-02-29 19:53:11 -0300

    Ended: challenge-Encryptor

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ function enableButtons() {
   if (texto != "") {
     document.getElementById("btnEncrypt").removeAttribute("disabled");
     document.getElementById("btnDecrypt").removeAttribute("disabled");
+    asignTextToElement("p", texto);
+    showInitials(false);
   } else {
     document.getElementById("btnEncrypt").setAttribute("disabled", "true");
     document.getElementById("btnDecrypt").setAttribute("disabled", "true");
     asignTextToElement("p", "");
+    showInitials(true);
   }
 }
 
@@ -24,6 +27,7 @@ function encrypt() {
   let text = document.getElementById("textInput").value;
   showInitials(false);
   asignTextToElement("p", encryptText(text));
+  document.getElementById("btnCopyTxt").removeAttribute("hidden");
 }
 
 function decrypt() {
@@ -32,15 +36,23 @@ function decrypt() {
   asignTextToElement("p", decryptText(text));
 }
 
+function copyTextBtn() {
+  let text = document.querySelector("p[name='results']").innerText;
+  console.log(text);
+  document.getElementById("textInput").value = text;
+}
+
 function showInitials(value) {
   if (value === true) {
-    document.getElementById("imgPuppet").removeAttribute("hidden");
+    document.getElementById("imgBoy").removeAttribute("hidden");
     asignTextToElement("h3", area2msgNotFound);
     asignTextToElement("h6", area2msgQuestion);
+    document.getElementById("btnCopyTxt").setAttribute("hidden", "true");
   } else {
-    document.getElementById("imgPuppet").setAttribute("hidden", "true");
+    document.getElementById("imgBoy").setAttribute("hidden", "true");
     asignTextToElement("h3", "");
     asignTextToElement("h6", "");
+    document.getElementById("btnCopyTxt").removeAttribute("hidden");
   }
 }
 
@@ -118,18 +130,18 @@ La letra "u" es convertida para "ufat"
 
 Requisitos:
 
-Debe funcionar solo con letras minúsculas
-No deben ser utilizados letras con acentos ni caracteres especiales
-Debe ser posible convertir una palabra para la versión encriptada también devolver 
-una palabra encriptada para su versión original.
+Debe funcionar solo con letras minúsculas ok
+No deben ser utilizados letras con acentos ni caracteres especiales ok
+Debe ser posible convertir una palabra para la versión encriptada también devolver ok
+una palabra encriptada para su versión original. ok
 
 Por ejemplo:
 "gato" => "gaitober"
 gaitober" => "gato"
 
 La página debe tener campos para inserción del texto que será encriptado o desencriptado, y 
-el usuario debe poder escoger entre as dos opciones.
-El resultado debe ser mostrado en la pantalla.
+el usuario debe poder escoger entre as dos opciones. ok
+El resultado debe ser mostrado en la pantalla. ok
 
 Extras:
 Un botón que copie el texto encriptado/desencriptado para la sección de transferencia, o sea 
